b2a9ff8b8c64b3e7dfd6f909fd11fd47b4595ab5
AbelGAlem <abelzeraftechnologies@gmail.com>
2025-12-26 14:39:34 +0200

    refactor: change error handling logic

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { HTTPException } from "hono/http-exception"
 
 import { db } from "@/db/drizzle";
 import {accounts} from "@/db/schema"
+import { eq } from "drizzle-orm";
 
 const app = new Hono()
   .get("/", 
@@ -12,15 +12,14 @@ const app = new Hono()
       const auth = getAuth(c)
 
       if(!auth?.userId){
-        throw new HTTPException(401, {
-          res: c.json({error: "Dont have permision"}, 401)
-        })
+        return c.json({error: "Dont have permision"}, 401)
       }
 
       const data = await db.select({
         id: accounts.id,
         name: accounts.name
       }).from(accounts)
+      .where(eq(accounts.userId, auth.userId))
 
       return c.json({ accounts: data });
     });
