9f1d536d8afcd2f99087c95399e1564a8202690c
AbelGAlem <abelzeraftechnologies@gmail.com>
2025-12-26 14:34:32 +0200

    feat: add tanstack query and hono client

---
+++ features/accounts/api/use-get-accounts.tsx
@@ -0,0 +1,19 @@
+import { client } from "@/lib/hono-client";
+import { useQuery } from "@tanstack/react-query";
+
+export function useGetAccounts() {
+  const query = useQuery({
+    queryKey: ["accounts"],
+    queryFn: async () => {
+      const response = await client.api.accounts.$get()
+
+      if(!response.ok){
+        throw new Error("Error fetching accounts")
+      }
+
+      const {accounts} = await response.json();
+      return accounts;
+    }
+  })
+  return query;
+}
