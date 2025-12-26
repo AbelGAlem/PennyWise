b53cd37c5d8affa5e204619f37e7862af405a0ce
AbelGAlem <abelzeraftechnologies@gmail.com>
2025-12-27 00:09:48 +0200

    style: improve sheet styling for mobile

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
 
 export default function Home() {
+  const { onOpen } = useNewAccount();
   return (
     <div>
       <h1>Dashboard</h1>
+      <Button onClick={() => onOpen()}>
+        Add Account
+      </Button>
     </div>
   );
 }
