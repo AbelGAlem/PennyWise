04285396957d5ac9811c5611e2faed2881b43b80
AbelGAlem <abelzeraftechnologies@gmail.com>
2025-12-27 12:36:38 +0200

    feat: add accounts page with tanstack table and delete feature

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,51 +1,204 @@
 import { Hono } from "hono";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-
 import { db } from "@/db/drizzle";
-import {accounts, insertAccountsSchema} from "@/db/schema"
-import {zValidator} from "@hono/zod-validator"
-import { eq } from "drizzle-orm";
-
+import { and, eq, inArray } from "drizzle-orm";
+import { accounts, insertAccountsSchema } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
 import { v4 as uuidv4 } from 'uuid';
+import { z } from "zod";
 
 const app = new Hono()
-  .get("/", 
-    clerkMiddleware(),
-    async (c) => {
-      const auth = getAuth(c)
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
 
-      if(!auth?.userId){
-        return c.json({error: "Dont have permision"}, 401)
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
+
+    return c.json({ data });
+  })
+  .get(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+
+      if (!id) {
+        return c.json({ error: "Missing id" }, 400);
       }
 
-      const data = await db.select({
-        id: accounts.id,
-        name: accounts.name
-      }).from(accounts)
-      .where(eq(accounts.userId, auth.userId))
+      if (!auth?.userId) {
+        c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const userId = auth?.userId as string;
+
+      const [data] = await db
+        .select({
+          id: accounts.id,
+          name: accounts.name,
+        })
+        .from(accounts)
+        .where(and(eq(accounts.userId, userId), eq(accounts.id, id)));
+
+      if (!data) {
+        return c.json({ error: "Not found" }, 404);
+      }
 
       return c.json({ data });
-    })
-    .post("/",
-      clerkMiddleware(),
-      zValidator("json", insertAccountsSchema.pick({
-        name: true
-      })),
-      async (c) => {
-        const auth = getAuth(c)
-        const values = c.req.valid("json")
+    }
+  )
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      insertAccountsSchema.pick({
+        name: true,
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
 
-        if(!auth?.userId){
-          return c.json({error: "Unauthorized"}, 401)
-        }
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
 
-        const [data] = await db.insert(accounts).values({
+      const [data] = await db
+        .insert(accounts)
+        .values({
           id: uuidv4(),
           userId: auth.userId,
-          ...values
-        }).returning()
+          ...values,
+        })
+        .returning();
 
-        return c.json({data})
-    })
+      return c.json({ data });
+    }
+  )
+  .post(
+    "/bulk-delete",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      z.object({
+        ids: z.array(z.string()),
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const data = await db
+        .delete(accounts)
+        .where(
+          and(
+            eq(accounts.userId, auth.userId),
+            inArray(accounts.id, values.ids)
+          )
+        )
+        .returning({
+          id: accounts.id,
+        });
+
+      return c.json({ data });
+    }
+  )
+  .patch(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+    zValidator(
+      "json",
+      insertAccountsSchema.pick({
+        name: true,
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+      const values = c.req.valid("json");
+
+      if (!id) {
+        return c.json({ error: "Missing id" }, 400);
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const [data] = await db
+        .update(accounts)
+        .set(values)
+        .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)))
+        .returning();
+
+      if (!data) {
+        return c.json({ error: "Not found" }, 404);
+      }
+
+      return c.json({ data });
+    }
+  )
+  .delete(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+
+      if (!id) {
+        return c.json({ error: "Missing id" }, 400);
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const [data] = await db
+        .delete(accounts)
+        .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)))
+        .returning({
+          id: accounts.id
+        } );
+
+      if (!data) {
+        return c.json({ error: "Not found" }, 404);
+      }
+
+      return c.json({ data });
+    }
+  );
 
 export default app;
