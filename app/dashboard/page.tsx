1005a75c701562ab08b0a148e57f95ed3eaa63b1
AbelGAlem <abelzeraftechnologies@gmail.com>
2026-02-13 18:54:32 +0200

    fix: wrapping in suspense for nextjs static generation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import { DataCharts } from "@/components/data-charts";
 import { DataGrid } from "@/components/data-grid";
 
 export default function DashboardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
-      <DataGrid />
-      <DataCharts />
+      <Suspense>
+        <DataGrid />
+        <DataCharts />
+      </Suspense>
     </div>
   );
 }
